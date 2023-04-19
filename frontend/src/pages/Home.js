fdb4f8aa125d5af1919b11265c0ab6fa2695d1b1
hyosin <>
2023-04-20 05:16:31 +0900

    feat: home 페이지 마크업

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,34 @@
+import Header from "../components/Header"
+import Aboutme from "../components/Aboutme"
+import WhoamI from "../components/WhoamI"
+import Timeline from "../components/Timeline"
+import Project from "../components/Project"
+import Footer from "../components/Footer"
+
+import styled from "styled-components"
+import {flexCenter} from "../styles/theme"
+
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<Wrapper>
+			<Header />
+			<Aboutme />
+			<WhoamI />
+			<Timeline />
+			<Project />
+			<Footer />
+		</Wrapper>
+	)
 }
 
 export default Home
+
+const Wrapper = styled.main`
+	display: flex;
+	margin: auto;
+	flex-direction: column;
+	max-width: 80rem;
+	background-color: pink;
+	height: calc(100vh - 60px);
+	margin-top: 60px;
+`
