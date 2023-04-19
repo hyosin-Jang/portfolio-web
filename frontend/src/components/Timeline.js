1f3e66f94eb7bfa1e1f047833a0b5c83ae56b07d
hyosin <>
2023-04-20 05:16:56 +0900

    feat: timeline 섹션 마크업 (chartjs 추가 예정)

--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -1,6 +1,11 @@
+import styled from "styled-components"
+
 // TODO: 그래프에 chart.js 사용하기
 const Timeline = () => {
-	return <div>Timeline</div>
+	return <Wrapper>Chart.js..</Wrapper>
 }
-
 export default Timeline
+
+const Wrapper = styled.div`
+	display: flex;
+`
