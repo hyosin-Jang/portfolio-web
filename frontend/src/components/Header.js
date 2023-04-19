faf95f711c88399761fef89b8b92ada840081745
hyosin <>
2023-04-20 05:16:07 +0900

    feat: 헤더 마크업

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,29 @@
+import styled from "styled-components"
+
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<Wrapper>
+			<span className="home">Header</span>
+		</Wrapper>
+	)
 }
 
 export default Header
+
+const Wrapper = styled.header`
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: orange;
+	width: 100%;
+	height: 60px;
+
+	display: flex;
+	align-items: center;
+	padding: 0 1rem;
+
+	.home {
+		font-size: 2rem;
+		font-weight: 800;
+	}
+`
