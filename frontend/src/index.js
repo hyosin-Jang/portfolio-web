19befdbf063da476b059bb34f9ac4f1b4d037904
hyosin <>
2023-04-20 05:13:58 +0900

    style: 전역 스타일 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import {RecoilRoot} from "recoil"
+import {GlobalStyle} from "./styles/globalStyle"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 	<React.StrictMode>
-		<App />
+		<RecoilRoot>
+			<GlobalStyle />
+			<App />
+		</RecoilRoot>
 	</React.StrictMode>
 )
 
