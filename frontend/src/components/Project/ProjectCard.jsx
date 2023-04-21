4f885eb29393257cea8a1c9f90476a4ad8eb4a43
hyosin <>
2023-04-21 16:55:40 +0900

    feat: 프로젝트 아이콘 클릭 시 모달창 추가

---
+++ frontend/src/components/Project/ProjectCard.jsx
@@ -0,0 +1,85 @@
+import {useState} from "react"
+
+import styled from "styled-components"
+import ScrollSlideAnimation from "../ScrollSlideAnimation"
+import Modal from "../Modal"
+import ProjectModalContent from "./ProjectModalContent"
+
+const ProjectCard = ({id, projectName, stack, description, projectIcon}) => {
+	const [isOpen, toggleOpen] = useState(false)
+	const onClose = () => toggleOpen((prev) => !prev)
+
+	return (
+		<Wrapper>
+			<ScrollSlideAnimation>
+				<Modal
+					isOpen={isOpen}
+					onClose={onClose}>
+					<ProjectModalContent />
+				</Modal>
+				<img
+					onClick={onClose}
+					className="app-icon"
+					src={projectIcon}
+					alt="app-icon"
+				/>
+
+				<div className="project-data">
+					<div className="project-name">{projectName}</div>
+					<div className="project-stack">
+						{stack &&
+							stack.map((stackName, idx) => (
+								<span
+									key={idx}
+									className="stack">
+									{stackName}
+								</span>
+							))}
+					</div>
+					<div className="project-description">{description}</div>
+				</div>
+			</ScrollSlideAnimation>
+		</Wrapper>
+	)
+}
+
+export default ProjectCard
+
+const Wrapper = styled.div`
+	display: flex;
+	min-height: 40rem;
+	.app-icon {
+		width: 10rem;
+		height: 10rem;
+		border-radius: 1.2rem;
+	}
+
+	.project-data {
+		margin-left: 3rem;
+		.project-name {
+			font-size: 2rem;
+			font-weight: 800;
+			margin-bottom: 0.8rem;
+		}
+		.project-stack {
+			display: flex;
+			margin-bottom: 0.7rem;
+			.stack {
+				color: #c28eec;
+				background: rgba(189, 66, 238, 0.212);
+				border-radius: 1.25rem;
+				padding: 0 0.6rem;
+				margin-right: 0.5rem;
+				display: flex;
+				min-width: 2rem;
+				justify-content: center;
+				font-size: 1rem;
+				line-height: 1.4rem;
+				margin-right: 0.6rem;
+			}
+		}
+		.project-description {
+			font-size: 1.5rem;
+		}
+	}
+`
---
+++ frontend/src/components/Project/ProjectModalContent.jsx
@@ -0,0 +1,102 @@
+import styled from "styled-components"
+
+// 모달창 안에 들어가는 내용
+// project:projectId로 최초 호출
+// 그 이후에는 각각 업데이트
+
+const dummy = {
+	projectName: "밥약",
+	duration: "2023.1 ~2023.4",
+	stack: [
+		{
+			stackId: 1,
+			stackName: "javascript",
+		},
+		{
+			stackId: 2,
+			stackName: "mysql",
+		},
+		{
+			stackId: 3,
+			stackName: "nodejs",
+		},
+		{
+			stackId: 4,
+			stackName: "sass",
+		},
+	],
+	view: 2, // 조회수
+	like: 10, // 좋아요
+	comment: [
+		{
+			commentId: 1,
+			comment: "우와~~~",
+			created_at: "2023.04.21 12:39:00",
+			name: "익명1",
+		},
+		{
+			commentId: 2,
+			comment: "우와~~잘보고갑니다~",
+			created_at: "2023.04.21 12:39:00",
+			name: "익명의사용자",
+		},
+		{
+			commentId: 3,
+			comment: "우와~~잘보고갑니다~",
+			created_at: "2023.04.21 12:39:00",
+			name: "하하하",
+		},
+		{
+			commentId: 4,
+			comment: "우와~~잘보고갑니다~",
+			created_at: "2023.04.21 12:39:00",
+			name: "야생의융콘",
+		},
+	],
+}
+
+const ProjectModalContent = () => {
+	const {projectName, duration, stack, view, like, comment} = dummy
+
+	return (
+		<Wrapper>
+			<div className="project-name">{projectName}</div>
+			<div className="duration">
+				<span>기간</span>
+				<span>duration</span>
+			</div>
+			<div className="stack">
+				<span>스택</span>
+				{stack && stack.map((s) => <span key={s.stackId}>{s.stackName}</span>)}
+			</div>
+			<div className="duration">
+				<span>기간</span>
+				<span>{duration}</span>
+			</div>
+			<div className="duration">
+				<span>조회수</span>
+				<span>{view}</span>
+				<span>좋아요</span>
+				<span>{like}</span>
+			</div>
+			<div className="comment">
+				<span>댓글</span>
+				{comment &&
+					comment.map((c) => (
+						<div key={c.commentId}>
+							<span>이름</span>
+							<span>{c.name}</span>
+							<span>내용</span>
+							<span>{c.comment}</span>
+							<span>작성일</span>
+							<span>{c.created_at}</span>
+						</div>
+					))}
+			</div>
+		</Wrapper>
+	)
+}
+
+export default ProjectModalContent
+
+const Wrapper = styled.header``
