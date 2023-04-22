6e30e44c1c368ebc4298f8e5df85cc411f98a151
hyosin <>
2023-04-22 19:33:05 +0900

    feat: 댓글 작성, 프로젝트별 댓글들 조회 api 추가

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,17 +1,46 @@
 const dummy = require("./dummy")
+const Comment = require("../models/comment")
+
 const express = require("express")
 const router = express.Router()
 
-// GET comment - 댓글 정보만 조회할 때 사용
-router.get("/:projectId", (req, res) => {
-	res.send(dummy.comment)
+// POST /comment - 댓글 등록
+router.post("/:projectId", async (req, res) => {
+	const projectId = req.params.projectId
+	const {name, content} = req.body
+
+	try {
+		const comment = await Comment.create({
+			fk_project_id: projectId,
+			name: name,
+			comment: content,
+		})
+
+		console.log("방금 만든 코멘트", comment)
+		res.status(201).json(comment)
+	} catch (err) {
+		console.error(err)
+		next(err)
+	}
 })
 
-// POST comment - 댓글 등록
-router.post("/:projectId", (req, res) => {
-	// 등록
-	// 업데이트된 결과 반환
-	res.send(dummy.comment)
+// GET /comment - 댓글 정보만 조회할 때 사용
+router.get("/:projectId", async (req, res) => {
+	const projectId = req.params.projectId
+
+	try {
+		const comments = await Comment.findAll({
+			where: {
+				fk_project_id: projectId,
+			},
+		})
+
+		console.log("프로젝트 id로 comments 조회", comments)
+		res.status(201).json(comments)
+	} catch (err) {
+		console.error(err)
+		next(err)
+	}
 })
 
 module.exports = router
