d4eb8d34fa465cbe223e94c880c001ac624761a2
hyosin <>
2023-04-22 19:29:58 +0900

    feat: comment 모델 추가

---
+++ backend/models/comment.js
@@ -0,0 +1,40 @@
+const Sequelize = require("sequelize")
+
+module.exports = class Comment extends Sequelize.Model {
+	static init(sequelize) {
+		return super.init(
+			{
+				comment_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+				},
+				name: {
+					type: Sequelize.STRING(32),
+				},
+				comment: {
+					type: Sequelize.TEXT,
+				},
+				created_at: {
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.NOW,
+				},
+				fk_project_id: {
+					type: Sequelize.INTEGER,
+				},
+			},
+			{
+				sequelize,
+				timestamps: false,
+				modelName: "Comment",
+				tableName: "comment",
+				charset: "utf8",
+				collate: "utf8_general_ci",
+			}
+		)
+	}
+	static associate(db) {
+		db.Comment.belongsTo(db.Project, {foreignKey: "fk_project_id", targetKey: "project_id"})
+	}
+}
