7bbb8f23742b0bc260d0c05032fa8a6a5be3460b
hyosin <>
2023-04-24 04:37:38 +0900

    style: 모달창 스타일 수정

--- frontend/src/components/Project/ProjectModalContent.jsx
+++ frontend/src/components/Project/ProjectModalContent.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react"
 import Comment from "./Comment"
 import {getProjectDetail, getProjectDetailImage, postView, postLike} from "../../api/projects"
 import styled from "styled-components"
+import {theme} from "../../styles/theme"
 import Slider from "./Slider"
 
 const ProjectModalContent = ({projectId}) => {
@@ -43,29 +44,46 @@ const ProjectModalContent = ({projectId}) => {
 						<Slider images={projectDetailImages} />
 						<div className="project-info">
 							<div className="project-row">
-								<Box>
+								<Box className="wrap">
 									<div className="box-title">기간</div>
 									<div className="box-content">
-										{projectDetail.start_date} - {projectDetail.end_date}
+										{projectDetail.start_date} ~ <br />
+										{projectDetail.end_date}
+									</div>
+								</Box>
+								<Box className="wrap">
+									<div className="box-title">스택</div>
+									<div className="box-content wrap">
+										{projectDetail.Stacks.length > 0 &&
+											projectDetail.Stacks.map((s) => (
+												<span
+													key={s.stack_id}
+													className="stack">
+													{s.stack_name}
+												</span>
+											))}
 									</div>
 								</Box>
 								<Box>
-									<div className="box-title">스택</div>
-									<div className="box-content">{projectDetail.Stacks.length > 0 && projectDetail.Stacks.map((s) => <span key={s.stack_id}>{s.stack_name}</span>)}</div>
-								</Box>
-								<Box>
 									<div className="box-title">링크</div>
-									<div className="box-content">{projectDetail.link}</div>
-								</Box>
-								<Box>
-									<div className="box-title">조회수</div>
-									<div className="box-content"> {projectDetail.view}</div>
+									<div className="box-content link wrap">
+										<a
+											className="link"
+											href={`${projectDetail.link}`}
+											target="_blank">
+											Click here!
+										</a>
+									</div>
 								</Box>
 							</div>
 							<div className="project-row">
-								<Box className="last-child">
+								<Box className="last-child description">
 									<div className="box-title">프로젝트 설명</div>
 									<div className="box-content"> {projectDetail.description}</div>
+								</Box>
+								<Box className="view">
+									<div className="box-title">조회수</div>
+									<div className="box-content"> {projectDetail.view}</div>
 								</Box>
 							</div>
 						</div>
@@ -106,27 +124,78 @@ const Wrapper = styled.div`
 `
 
 const Box = styled.div`
+	background-color: ${theme.colors.lightGrey};
+	padding: 1rem;
+	border-radius: 1.6rem;
+	max-width: 10rem;
+	margin: 0.5rem;
 	display: flex;
 	flex: 1;
 	flex-direction: column;
 	line-height: 1.5;
 	margin-top: 1rem;
 
+	&.view {
+		max-width: 5rem;
+	}
+
 	.box-title {
 		margin-left: 0.8rem;
-		font-size: 1.5rem;
-		font-weight: 600;
+		font-size: 1.2rem;
+		font-weight: 700;
 	}
 	.box-content {
-		padding: 0.4rem;
-		margin: 0.4rem;
+		height: 100%;
+		max-width: 30rem;
 		text-align: center;
+		color: ${theme.colors.drakGrey};
+		font-weight: 600;
 		margin-top: 0.1rem;
-		background-color: lightgrey;
 		border-radius: 2rem;
 
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		display: inline-block;
+
+		.stack {
+			margin-right: 0.2rem;
+			border-radius: 1.25rem;
+			padding: 0 0.2rem;
+			display: inline-block;
+		}
+
+		.wrap {
+			word-wrap: break-word;
+			overflow-wrap: break-word;
+		}
+
+		.link {
+			color: ${theme.colors.drakGrey};
+
+			&:active {
+				color: white;
+			}
+
+			&:hover {
+				color: ${theme.colors.lightPurple};
+				transition: color 0.3s ease-out;
+			}
+		}
+	}
+
+	.link {
+		max-width: 5rem;
+		display: inline-block;
+	}
+
+	.wrap {
+		word-wrap: break-word;
+		overflow-wrap: break-word;
+	}
+
+	&.description {
+		max-width: 30rem;
+		flex: 2;
 	}
 `
